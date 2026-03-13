b0fabc2febf41d565111002082c80749a516aa00
wiktor <wiktor.szepit@>
2026-03-13 11:13:51 +0100

    Add logic file, update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,83 @@
+import {useEffect, useState} from "react";
 import "./App.css";
+import { calculatePrice } from "./logika";
 
 function App() {
+    const [rodzaj, setRodzaj] = useState("kajak");
+    const [godziny, setGodziny] = useState(1);
+    const [kapoki, setKapoki] = useState(false);
+    const [instruktor, setInstruktor] = useState(false);
+    const [platnosc, setPlatnosc] = useState("blik");
+    const [wynik, setWynik] = useState<number | null>(null);
+    useEffect(() => {
+        if (rodzaj === "lodz") {
+            alert("Do prowadzenia żaglówki Omega wymagany jest patent żeglarski!");
+        }
+    }, [rodzaj]);
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        const cena = calculatePrice(rodzaj, godziny, kapoki, instruktor);
+        setWynik(cena);
+    };
+
     return (
         <div className="container">
             <h1>Mazurska Przystań – Rezerwacja</h1>
 
-            <div className="form">
+            <form className="form" onSubmit={handleSubmit}>
                 <label>
                     Wybierz łódkę:
-                    <select>
-                        <option>Kajak – 40 zł</option>
-                        <option>Rower wodny – 60 zł</option>
-                        <option>Łódź wiosłowa – 120 zł</option>
+                    <select value={rodzaj} onChange={(e) => setRodzaj(e.target.value)}>
+                        <option value="kajak">Kajak – 20 zł</option>
+                        <option value="rower">Rower wodny – 35 zł</option>
+                        <option value="lodz" >Flagowa Lodz Omega (zaglowka) – 150 zł</option>
                     </select>
                 </label>
 
                 <label>
-                    Liczba godzin:
-                    <input type="range" min={1} max={10} />
+                    Liczba godzin: {godziny}
+                    <input
+                        type="range"
+                        min={1}
+                        max={8}
+                        value={godziny}
+                        onChange={(e) => setGodziny(Number(e.target.value))}
+                    />
                 </label>
 
                 <label className="checkbox">
-                    <input type="checkbox" />
-                    Kapoki (+20 zł)
+                    <input
+                        type="checkbox"
+                        checked={kapoki}
+                        onChange={(e) => setKapoki(e.target.checked)}
+                    />
+                    Kapok dla dziecka (+5 zł)
                 </label>
 
                 <label className="checkbox">
-                    <input type="checkbox" />
-                    Instruktor (+50 zł)
+                    <input
+                        type="checkbox"
+                        checked={instruktor}
+                        onChange={(e) => setInstruktor(e.target.checked)}
+                    />
+                    Instruktor(+50 zł/h)
+                </label>
+                <label className={"radio"}>
+                    <input type="radio" name="radio" value="blik"  onChange={(e) => setPlatnosc(e.target.value)}/>Blik
+                    <input type="radio" name="radio" value="karta" onChange={(e) => setPlatnosc(e.target.value)}/>Karta
+
                 </label>
 
-                <button>Oblicz cenę</button>
+
+                <button type="submit">Oblicz cenę</button>
 
                 <div className="result">
-                    <h2>Cena końcowa : — zł</h2>
+                    <h2>
+                        Cena końcowa: {wynik !== null ? `${wynik} zł \n platnosc za pomoca: ${platnosc}` : "— zł"}
+                    </h2>
                 </div>
-            </div>
+            </form>
         </div>
     );
 }
